627370097c5e38d71c4a48817867225b6de862cb
Ivan Adamov <seventhdimensiongames@gmail.com>
2025-12-30 18:07:14 +0100

    test

--- AIController.cpp
+++ AIController.cpp
@@ -273,7 +273,7 @@ bool AIController::takeTurn(
         break;
 
     case ShotResult::Miss:
-        // ВАЖНО: если мы охотились — НЕ теряем корабль
+        // если мы охотились — НЕ теряем корабль
         if (!state_.hitCells.empty()) {
             resetDirectionAndRebuildTargets(playerBoard, shots);
         }
